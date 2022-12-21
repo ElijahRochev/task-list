093ad0859b1fb09b039e80d34c3548c48b343c10
Elijah Rochev <eliajhrochev@gmail.com>
2022-12-21 17:31:04 +0200

    add TaskForm component

--- src/App.js
+++ src/App.js
@@ -2,26 +2,16 @@ import MyInput from "./components/Ipnut/MyInput";
 import TaskList from "./components/TaskList";
 import "./style/app.css"
 import { useState } from "react";
+import TaskForm from "./components/TaskForm";
 function App() {
   const [tasks, setTasks] = useState([
-    {title: "wait"},
-    {title: "work"},
-
-
   ])
-  const [title, setTitle] = useState([])
-
-  const addNewTask = (e) => {
-    e.preventDefault()
-    const newTask = {
-      title
-    }
-    setTasks([...tasks, newTask])
-    setTitle("")
-  }
 
   function deleteTask(task) {
     setTasks(tasks.filter(t => t.title !== task.title))
+  }
+  const createTask = (newTask) => {
+    setTasks([...tasks, newTask])
   }
 
   return (
@@ -29,15 +19,7 @@ function App() {
     <div className="App">
       <div className="taskList">
         <h1>todos</h1>
-        <form onSubmit={addNewTask}>
-          <MyInput 
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-            type="text" 
-            placeholder="Name"
-            
-          />
-        </form>
+        <TaskForm create={createTask}/>
         <TaskList remove={deleteTask} tasks={tasks}/>
       </div>
     </div>
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MyInput from './Ipnut/MyInput'
 
-export default function TaskForm(e) {
-/*   const addTask = (e) => {
-    e.preventDefault()
+export default function TaskForm({create}) {
+  const [title, setTitle] = useState([])
 
+  const addNewTask = (e) => {
+    e.preventDefault()
+    const newTask = {
+      ...title
+    }
+    create(newTask)
+    setTitle({title: " "})
   }
-  const inputRef = useRef(); */
+
   return (
-    <form>
-        <MyInput 
-        type="text" 
-        placeholder="Name"
-        />
-    </form>
+    <form onSubmit={addNewTask}>
+          <MyInput 
+            value={title.title}
+            onChange={e => setTitle({...title,  title: e.target.value})}
+            type="text" 
+            placeholder="Name"
+            
+          />
+        </form>
   )
 }
